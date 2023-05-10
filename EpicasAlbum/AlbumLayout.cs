3e88e09e1f21b00709e6fd5ab491365fb7d1e1cd
Damián Garro <dgarro@dc.uba.ar>
2023-05-10 00:08:07 -0300

    UI adjustments

--- EpicasAlbum/AlbumLayout.cs
+++ EpicasAlbum/AlbumLayout.cs
@@ -5,7 +5,7 @@ namespace EpicasAlbum;
 
 public class AlbumLayout : MonoBehaviour
 {
-    private static Color DEFAULT_BORDER = new Color(0.3882f, 0.498f, 0.8431f);
+    private static Color DEFAULT_BORDER = new(0.3882f, 0.498f, 0.8431f);
     private const int HORIZONTAL_OFFSET = 50;
     private const int BIG_IMAGE_SIZE = 470;
     private const int GRID_IMAGE_SIZE = 100;
@@ -16,12 +16,21 @@ public class AlbumLayout : MonoBehaviour
     public static void Create(GameObject canvas)
     {
         GameObject albumLayout = new GameObject("AlbumLayout");
+        RectTransform albumLayoutRect = albumLayout.AddComponent<RectTransform>();
         albumLayout.transform.parent = canvas.transform; // TODO: Outside?
+        albumLayoutRect.localPosition = Vector3.zero;
+        albumLayoutRect.localEulerAngles = Vector3.zero;
+        albumLayoutRect.localScale = Vector3.one;
+        albumLayoutRect.anchorMin = Vector2.zero;
+        albumLayoutRect.anchorMax = Vector2.one;
+        albumLayoutRect.pivot = new Vector2(0.5f, 0.5f);
+        albumLayoutRect.offsetMin = Vector2.zero;
+        albumLayoutRect.offsetMax = Vector2.zero;
 
         BorderedImage borderedImage = BorderedImage.Create();
         borderedImage.SetBorderColor(DEFAULT_BORDER);
 
-        BorderedImage bigImage = Instantiate(borderedImage, canvas.transform); // TODO: albumLayout (make it stretch etc)
+        BorderedImage bigImage = Instantiate(borderedImage, albumLayout.transform);
         bigImage.name = "BigImage";
         RectTransform bigImageRect = bigImage.GetComponent<RectTransform>();
         // Centered vertically, offset from right
@@ -33,11 +42,10 @@ public class AlbumLayout : MonoBehaviour
 
         GameObject gridGo = new GameObject("Grid", typeof(GridLayoutGroup));
         RectTransform gridRect = gridGo.GetComponent<RectTransform>();
-        gridRect.parent = canvas.transform; // TODO: albumLayout (make it stretch etc)
-        // TODO: NOT WORKING
-        // gridRect.position = Vector3.zero;
-        // gridRect.eulerAngles = Vector3.zero;
-        // gridRect.localScale = Vector3.one;
+        gridRect.parent = albumLayout.transform;
+        gridRect.localPosition = Vector3.zero;
+        gridRect.localEulerAngles = Vector3.zero;
+        gridRect.localScale = Vector3.one;
         // Centered vertically, offset from left
         gridRect.anchorMin = new Vector2(0, 0.5f);
         gridRect.anchorMax = new Vector2(0, 0.5f);
@@ -53,7 +61,8 @@ public class AlbumLayout : MonoBehaviour
         grid.constraintCount = GRID_COLUMNS;
         for (int i = 0; i < GRID_ROWS * GRID_COLUMNS; i++)
         {
-            Instantiate(borderedImage, gridRect);
+            BorderedImage gridImage = Instantiate(borderedImage, gridRect);
+            gridImage.name = "GridImage_" + i;
         }
     }
 }
